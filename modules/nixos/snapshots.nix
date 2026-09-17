f5c8f0e6623b25c525f865d8a03f009ff9aa4a1c
RSTLSS <72990855+rst-lss@users.noreply.github.com>
2026-09-17 18:41:36 +0330

    nixos/snapshots: reformat to match nix formatting convention

--- modules/nixos/snapshots.nix
+++ modules/nixos/snapshots.nix
@@ -1,18 +1,16 @@
-{ ... }:
-
-{
+_: {
   services.snapper = {
     configs = {
       root = {
         SUBVOLUME = "/";
-        ALLOW_USERS = [ "rstlss" ];
+        ALLOW_USERS = ["rstlss"];
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
       };
 
       home = {
         SUBVOLUME = "/home";
-        ALLOW_USERS = [ "rstlss" ];
+        ALLOW_USERS = ["rstlss"];
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
       };
