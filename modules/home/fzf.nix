74d66135d95da014800c1e579451c13ea81b1e70
RSTLSS <72990855+rst-lss@users.noreply.github.com>
2026-09-22 11:09:26 +0330

    modules/home/fzf: extend file search to automounted external drives on linux

--- modules/home/fzf.nix
+++ modules/home/fzf.nix
@@ -1,12 +1,21 @@
-_: {
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  isLinux = pkgs.stdenv.isLinux;
+  mediaDir = "/run/media/${config.home.username}";
+  searchPaths = "$HOME" + lib.optionalString isLinux " ${mediaDir}";
+in {
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
     tmux.enableShellIntegration = true;
 
     changeDirWidgetCommand = "fd --type d --hidden --exclude .git . $HOME";
-    defaultCommand = "fd --type f --hidden --exclude .git . $HOME";
-    fileWidgetCommand = "fd --type f --hidden --exclude .git . $HOME";
+    defaultCommand = "fd --type f --hidden --exclude .git . ${searchPaths} 2>/dev/null";
+    fileWidgetCommand = "fd --type f --hidden --exclude .git . ${searchPaths} 2>/dev/null";
     defaultOptions = ["--ansi"];
   };
 }
